aea3f3594bc9782fbd6638468e22a043da25732e
Paul James Curacha <kamcuracha@yahoo.com>
2017-10-04 17:35:14 +0800

    Finish content, slider start

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    console.log('n is: ' + n);
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slider");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = slides.length} 
+    if (n < 1) {slideIndex = 1}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block"; 
+    dots[slideIndex-1].className += " active";
+}
